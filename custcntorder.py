48a67279858192b36886e9dbf4bfcd84e6054c27
Prmnk <pramanikota@gmail.com>
2020-10-20 02:18:00 +0530

    new transform

--- custcntorder.py
+++ custcntorder.py
@@ -1,17 +1,19 @@
 from pyspark import SparkConf, SparkContext
-conf = SparkConf().setMaster("local").setAppName("countcust")
+conf = SparkConf().setMaster("local").setAppName("countcur")
 
-sc = SparkContext(conf = conf)
+sc= SparkContext(conf = conf)
 
-def parse(line):
+def parseline(line):
     f1 = int(line.split(',')[0])
     f2 = float(line.split(',')[1])
     return f1, f2
 
 data = sc.textFile("file:///sparkcourse/customer-orders.csv")
-cnt = data.map(parse)
-cntbycust = cnt.reduceByKey(lambda x,y: x+y)
-cntsort = cntbycust.map(lambda x : (x[1],x[0])).sortByKey(ascending = False)
+
+cnt = data.map(parseline)
+cnt_bycust = cnt.reduceByKey(lambda x,y : x+y)
+cntsort = cnt_bycust.map(lambda x: ( x[1], x[0] ) ).sortByKey(ascending = False)
+
 results = cntsort.collect()
 
 for x in results:
